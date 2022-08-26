da332617e5fd9a580ddcd6a96e0579a1e38b3fcd
HyunJun Kim <denny10002@naver.com>
2022-08-26 16:28:26 +0900

    check Main Page Data

--- server/app.js
+++ server/app.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://lab0104:lab0104@lab0104.sqskoaj.mongodb.net/test";
+
+const options = {useUnifiedTopology: true};
+
+ 
+
+MongoClient.connect(url, options, function(err, db) {
+
+  if (err) throw err;
+
+  var dbo = db.db("lab0104");
+
+  // function TotalParking(TPL) {
+  //   console.log("2층 총 주차공간: " + TPL);
+  // } 
+
+  dbo.collection("parking").find({observationtime: '2022082422'}, { projection: { _id: 0, TotalParkingLot: 1,
+     availableparking: 1, emptyparking:1, date: 1} }).toArray(function(err, result) {
+    
+    if (err) throw err;
+    
+    // TotalParking(dbo.collection("parking").find());
+    
+    console.log(result);
+    realData = result;
+    db.close();
+
+  });
+
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(realData);  
 })
 
 app.listen(port, () => {
